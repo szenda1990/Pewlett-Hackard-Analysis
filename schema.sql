56d52d4566bb09fe1eab5e28ce7faae98c379282
szenda1990 <szenda1990@gmail.com>
2020-01-12 20:51:26 -0800

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 -- Creating tables for PH-EmployeeDB
+DROP TABLE departments CASCADE;
 CREATE TABLE departments (
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE departments (
 	UNIQUE (dept_name)
 );
 
+DROP TABLE employees CASCADE;
 CREATE TABLE employees (
 	emp_no INT NOT NULL,
 	birth_date DATE NOT NULL,
@@ -16,16 +18,18 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
+DROP TABLE dept_manager CASCADE;
 CREATE TABLE dept_manager (
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 
+DROP TABLE salaries CASCADE;
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
@@ -35,22 +39,23 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
+DROP TABLE dept_emp CASCADE;
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
+DROP TABLE titles CASCADE;
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
+	title VARCHAR(40) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-SELECT * FROM departments;
